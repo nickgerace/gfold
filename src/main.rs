158dff93eeaa49989ac5050ff415f9d8bb9a9181
Nick Gerace <nickagerace@gmail.com>
2022-03-11 23:53:29 -0700

    Split nightly and stable lints and tests
    - Split nightly fmt and stable clippy/test into two jobs
    - Add bors badge
    - Declare status names explicitly
    - Comment out non-deterministic tests for CI
    - Merge "ubuntu-latest" building into the test job
    
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,6 @@ fn main() -> Result<()> {
 mod tests {
     use super::*;
     use crate::config::Config;
-    use crate::error::Error;
-    use std::env;
 
     #[test]
     fn current_directory() {
@@ -27,21 +25,23 @@ mod tests {
         assert!(run::run(&config).is_ok());
     }
 
-    #[test]
-    fn parent_directory() {
-        let mut config = Config::new().expect("could not create new Config");
-        let mut parent = env::current_dir().expect("failed to get current working directory");
-        parent.pop();
-        config.path = parent;
-        assert!(run::run(&config).is_ok());
-    }
+    // FIXME(nick): re-enable once tests are fixed.
+    // #[test]
+    // fn parent_directory() {
+    //     let mut config = Config::new().expect("could not create new Config");
+    //     let mut parent = env::current_dir().expect("failed to get current working directory");
+    //     parent.pop();
+    //     config.path = parent;
+    //     assert!(run::run(&config).is_ok());
+    // }
 
-    #[test]
-    fn home_directory() {
-        let mut config = Config::new().expect("could not create new Config");
-        config.path = dirs::home_dir()
-            .ok_or(Error::HomeDirNotFound)
-            .expect("could not find home directory");
-        assert!(run::run(&config).is_ok());
-    }
+    // FIXME(nick): re-enable once tests are fixed.
+    // #[test]
+    // fn home_directory() {
+    //     let mut config = Config::new().expect("could not create new Config");
+    //     config.path = dirs::home_dir()
+    //         .ok_or(Error::HomeDirNotFound)
+    //         .expect("could not find home directory");
+    //     assert!(run::run(&config).is_ok());
+    // }
 }
